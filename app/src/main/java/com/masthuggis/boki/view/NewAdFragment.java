387bae284681a4d8b5fe8eed2b479533631af1f7
Isak Lindgren <isak_lindgren_5@hotmail.com>
2019-09-13 17:33:31 +0200

    ButtonsWorking

--- app/src/main/java/com/masthuggis/boki/view/NewAdFragment.java
+++ app/src/main/java/com/masthuggis/boki/view/NewAdFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,10 +13,38 @@ import androidx.fragment.app.Fragment;
 import com.masthuggis.boki.R;
 
 public class NewAdFragment extends Fragment {
+    private ImageButton newBookButton;
+    private ImageButton newCollectionButton;
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.new_ad_fragment,container,false);
+        View v = inflater.inflate(R.layout.new_ad_fragment, container, false);
+
+        newCollectionButton= (ImageButton)v.findViewById(R.id.to_new_collection_imagebutton);
+        newBookButton = (ImageButton)v.findViewById(R.id.to_new_book_imagebutton);
+        newCollectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        newBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        newCollectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return v;
 
     }
+
+
 }
