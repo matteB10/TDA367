e5e29d194521ba2794835a3fd9013a28f42ea3cd
Isak Lindgren <isak_lindgren_5@hotmail.com>
2019-09-16 12:50:44 +0200

    Firebase WIP

--- app/src/main/java/com/masthuggis/boki/backend/BackendDataFetcher.java
+++ app/src/main/java/com/masthuggis/boki/backend/BackendDataFetcher.java
@@ -1,6 +1,19 @@
 package com.masthuggis.boki.backend;
 
 import android.content.Context;
+import android.nfc.Tag;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +22,8 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 /**
  * Utility class with functionality for Fetching data from Backend in form of a .json-file.
@@ -22,6 +37,7 @@ public class BackendDataFetcher {
      * @param context the Context-object required to load the .json-file via the assets-folder
      * @return the contents of the loaded .json-file formatted as a String.
      */
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     static String loadJSONFromAsset(Context context) {
         String json = null;
@@ -39,4 +55,4 @@ public class BackendDataFetcher {
             return null;
         }
     }
-}
+    }
--- app/src/main/java/com/masthuggis/boki/view/MainActivity.java
+++ app/src/main/java/com/masthuggis/boki/view/MainActivity.java
@@ -8,28 +8,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.masthuggis.boki.R;
+import com.masthuggis.boki.backend.BackendDataFetcher;
+
+/**
+ * MainActivity is the main activity of the application. It is the center which everything else builds upon.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNav =findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        BackendDataFetcher backendDataFetcher = new BackendDataFetcher();
+        backendDataFetcher.loadJSONFromFB();
+
     }
+
+    /**
+     * @navListener Handles swapping between fragments when navigating through the bottom panel. It does this
+     * by using a OnNavigationItemSelectedListener which uses the items ID and a switch case.
+     */
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             Fragment selectedFragment = null;
 
-            switch(menuItem.getItemId()){
+            switch (menuItem.getItemId()) {
                 case R.id.navigation_favorites:
                     selectedFragment = new FavoritesFragment();
                     break;
@@ -47,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                     break;
 
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
             return true;
 
 
