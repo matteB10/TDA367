21075e234c37b1c694d8c5ad2d5ae065569af286
Joel Rudsberg <rudsberg@live.se>
2019-09-17 14:44:08 +0200

    removed unused code

--- app/src/test/java/com/masthuggis/boki/ExampleUnitTest.java
+++ app/src/test/java/com/masthuggis/boki/ExampleUnitTest.java
@@ -1,9 +1,5 @@
 package com.masthuggis.boki;
 
-import com.masthuggis.boki.backend.Repository;
-import com.masthuggis.boki.backend.iRepository;
-import com.masthuggis.boki.presenter.HomePresenter;
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -19,36 +15,4 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 
-    @Test
-    public void numAdvertsShow_IsSameAsRepository() {
-        /*
-        HomePresenter.View view = createHomeView();
-        iRepository repository = Repository.getInstance();
-
-        HomePresenter presenter = new HomePresenter(view, repository);
-        int numItems = repository.getAllAds().size();
-
-        assertEquals(presenter.getNumRows(), numItems);
-
-         */
-    }
-
-    private HomePresenter.View createHomeView() {
-        return new HomePresenter.View() {
-            @Override
-            public void showLoadingScreen() {
-
-            }
-
-            @Override
-            public void hideLoadingScreen() {
-
-            }
-
-            @Override
-            public void showDetailsScreen(long id) {
-
-            }
-        };
-    }
 }
