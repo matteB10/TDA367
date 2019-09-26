91d875380d53b793cacace4292989ed6f4cf1a08
Joel Rudsberg <rudsberg@live.se>
2019-09-26 16:37:03 +0200

    refactored MainActivity

--- app/src/main/java/com/masthuggis/boki/view/MainActivity.java
+++ app/src/main/java/com/masthuggis/boki/view/MainActivity.java
@@ -1,20 +1,12 @@
 package com.masthuggis.boki.view;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.masthuggis.boki.R;
-import com.masthuggis.boki.backend.Repository;
-import com.masthuggis.boki.model.Advert;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 /**
  * MainActivity is the primary view of the application. This is where the application will take you on launch.
@@ -38,33 +30,29 @@ public class MainActivity extends AppCompatActivity {
      * This is a method handling the navigation between fractals which are parts of the view of the mainActivity class.
      * It does this through the defined ID:s of the different fragments.
      */
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment selectedFragment = null;
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = menuItem -> {
+        Fragment selectedFragment;
 
-            switch(menuItem.getItemId()){
-                case R.id.navigation_favorites:
-                    selectedFragment = new FavoritesFragment();
-                    break;
-                case R.id.navigation_profile:
-                    selectedFragment = new ProfileFragment();
-                    break;
-                case R.id.navigation_new_ad:
-                    selectedFragment = new NewAdFragment();
-                    break;
-                case R.id.navigation_messages:
-                    selectedFragment = new MessagesFragment();
-                    break;
-                default:
-                    selectedFragment = new HomeFragment();
-
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
-            return true;
-
+        switch(menuItem.getItemId()){
+            case R.id.navigation_favorites:
+                selectedFragment = new FavoritesFragment();
+                break;
+            case R.id.navigation_profile:
+                selectedFragment = new ProfileFragment();
+                break;
+            case R.id.navigation_new_ad:
+                selectedFragment = new NewAdFragment();
+                break;
+            case R.id.navigation_messages:
+                selectedFragment = new MessagesFragment();
+                break;
+            default:
+                selectedFragment = new HomeFragment();
 
         }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+        return true;
+
 
     };
 
