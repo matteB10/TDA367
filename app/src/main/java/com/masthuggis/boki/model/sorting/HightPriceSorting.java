87fbe524e01f4e97f33a57afbc9321da90fecf9e
Joel Rudsberg <rudsberg@live.se>
2019-09-30 08:04:42 +0200

    Added sort logic for lowest price, highest price and latest published

--- app/src/main/java/com/masthuggis/boki/model/sorting/HightPriceSorting.java
+++ app/src/main/java/com/masthuggis/boki/model/sorting/HightPriceSorting.java
@@ -2,8 +2,11 @@ package com.masthuggis.boki.model.sorting;
 
 import com.masthuggis.boki.model.Advertisement;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class HightPriceSorting implements SortStrategy {
 
@@ -11,7 +14,10 @@ class HightPriceSorting implements SortStrategy {
 
     @Override
     public Iterator<Advertisement> sort(List<Advertisement> adverts) {
-        return adverts.iterator();
+        return new ArrayList<>(adverts).stream()
+                .sorted(Comparator.comparing(Advertisement::getPrice))
+                .collect(Collectors.toList())
+                .iterator();
     }
 
     @Override
--- app/src/main/java/com/masthuggis/boki/model/sorting/LowestPriceSorting.java
+++ app/src/main/java/com/masthuggis/boki/model/sorting/LowestPriceSorting.java
@@ -2,15 +2,21 @@ package com.masthuggis.boki.model.sorting;
 
 import com.masthuggis.boki.model.Advertisement;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class LowestPriceSorting implements SortStrategy {
     private static final String NAME = "Lägsta pris";
 
     @Override
     public Iterator<Advertisement> sort(List<Advertisement> adverts) {
-        return adverts.iterator();
+        return new ArrayList<>(adverts).stream()
+                .sorted(Comparator.comparing(Advertisement::getPrice).reversed())
+                .collect(Collectors.toList())
+                .iterator();
     }
 
     @Override
