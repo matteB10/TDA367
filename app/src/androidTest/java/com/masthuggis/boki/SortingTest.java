d4bd2e8e53b4f9272e5ab5102dc6360f81775455
Joel Rudsberg <rudsberg@live.se>
2019-10-02 12:13:20 +0200

    Added tests for reverse alphabetical and refactored to improve code quality using @before and helper methods

--- app/src/androidTest/java/com/masthuggis/boki/SortingTest.java
+++ app/src/androidTest/java/com/masthuggis/boki/SortingTest.java
@@ -7,6 +7,7 @@ import com.masthuggis.boki.model.Advertisement;
 import com.masthuggis.boki.model.sorting.SortFactory;
 import com.masthuggis.boki.model.sorting.SortStrategy;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,14 +18,38 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class SortingTest {
+    private List<Advertisement> adverts = new ArrayList<>();
+
+    @Before
+    public void before() {
+        adverts = MockRepository.getInstance().getLocalJSONAds();
+    }
+
+    private void sort(SortStrategy strategy) {
+        adverts = strategy.sort(adverts);
+    }
+
     @Test
     public void testAlphabeticalSortingIsCorrect() {
-        List<Advertisement> sorted = MockRepository.getInstance().getLocalJSONAds();
-        SortStrategy strategy = SortFactory.getAlphabeticalSorting();
+        sort(SortFactory.getAlphabeticalSorting());
 
-        sorted = strategy.sort(sorted);
+        assertEquals(adverts.get(0).getTitle().toLowerCase(), "arikitektur");
+        assertEquals(adverts.get(1).getTitle().toLowerCase(), "calculus");
+        assertEquals(adverts.get(2).getTitle().toLowerCase(), "clean code");
+        assertEquals(adverts.get(3).getTitle().toLowerCase(), "design");
+        assertEquals(adverts.get(4).getTitle().toLowerCase(), "diskret");
+        assertEquals(adverts.get(5).getTitle().toLowerCase(), "linjär algebra");
+    }
 
-        assertEquals(sorted.get(0).getTitle(), "Arikitektur");
-        assertEquals(sorted.get(1).getTitle(), "Calculus");
+    @Test
+    public void testReverseAlphabeticalSortingIsCorrect() {
+        sort(SortFactory.getReversedAlphabeticalSorting());
+        
+        assertEquals(adverts.get(0).getTitle().toLowerCase(), "linjär algebra");
+        assertEquals(adverts.get(1).getTitle().toLowerCase(), "diskret");
+        assertEquals(adverts.get(2).getTitle().toLowerCase(), "design");
+        assertEquals(adverts.get(3).getTitle().toLowerCase(), "clean code");
+        assertEquals(adverts.get(4).getTitle().toLowerCase(), "calculus");
+        assertEquals(adverts.get(5).getTitle().toLowerCase(), "arikitektur");
     }
 }
