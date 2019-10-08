c2f394b9dd4284fdc110780602ad331b74871a5f
Arvid Sundbom <suarvid@student.chalmers.se>
2019-10-08 12:10:59 +0200

    Fixed a bunch of stuff that was broken by merges

--- app/src/main/java/com/masthuggis/boki/presenter/CreateAdPresenter.java
+++ app/src/main/java/com/masthuggis/boki/presenter/CreateAdPresenter.java
@@ -3,6 +3,7 @@ package com.masthuggis.boki.presenter;
 import com.masthuggis.boki.backend.AdFactory;
 import com.masthuggis.boki.backend.Repository;
 import com.masthuggis.boki.model.Advertisement;
+import com.masthuggis.boki.model.DataModel;
 import com.masthuggis.boki.utils.CurrentTimeHelper;
 import com.masthuggis.boki.utils.FormHelper;
 import com.masthuggis.boki.utils.StylingHelper;
@@ -148,6 +149,7 @@ public class CreateAdPresenter {
     public void publishAdvert() {
         setAdvertDate();
         Repository.saveAdvert(view.getCurrentImageFile(), advertisement);
+
         advertisement = null;
     }
 
--- app/src/main/java/com/masthuggis/boki/view/CreateAdActivity.java
+++ app/src/main/java/com/masthuggis/boki/view/CreateAdActivity.java
@@ -156,8 +156,8 @@ public class CreateAdActivity extends AppCompatActivity implements CreateAdPrese
         BitmapFactory.Options imageOptions = new BitmapFactory.Options();
         imageOptions.inJustDecodeBounds = true;
         Bitmap bitmap = BitmapFactory.decodeFile(currentImageFile.getPath());
-        //Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 600, 800, true); //TODO change 200x200 to preferable resolution
-        return bitmap;
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 1080, 1920, false); //TODO is this even necessary?
+        return scaledBitmap;
     }
 
     private void setListeners() {
