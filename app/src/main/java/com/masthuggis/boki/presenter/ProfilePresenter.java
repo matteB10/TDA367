f98b405f5b94a4a8a251440b0e57d012fb83a39e
Isak Lindgren <isak_lindgren_5@hotmail.com>
2019-10-11 21:45:20 +0200

    removed android dependency from a presenter

--- app/src/main/java/com/masthuggis/boki/presenter/ProfilePresenter.java
+++ app/src/main/java/com/masthuggis/boki/presenter/ProfilePresenter.java
@@ -3,7 +3,6 @@ package com.masthuggis.boki.presenter;
 import com.masthuggis.boki.backend.callbacks.advertisementCallback;
 import com.masthuggis.boki.model.Advertisement;
 import com.masthuggis.boki.model.DataModel;
-import com.masthuggis.boki.model.observers.AdvertisementObserver;
 import com.masthuggis.boki.model.sorting.SortManager;
 
 import java.util.List;
