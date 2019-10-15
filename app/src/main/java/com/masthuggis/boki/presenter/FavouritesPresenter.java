d053b2e48440c635d404e56e10ebe337cfb168c1
Joel Rudsberg <rudsberg@live.se>
2019-10-15 14:03:06 +0200

    Refactored so favorites view and ppresenter uses the new improved design

--- app/src/main/java/com/masthuggis/boki/presenter/FavouritesPresenter.java
+++ app/src/main/java/com/masthuggis/boki/presenter/FavouritesPresenter.java
@@ -2,51 +2,24 @@ package com.masthuggis.boki.presenter;
 
 import com.masthuggis.boki.model.Advertisement;
 import com.masthuggis.boki.model.DataModel;
-import com.masthuggis.boki.utils.ClickDelayHelper;
-import com.masthuggis.boki.view.ThumbnailView;
+import com.masthuggis.boki.model.sorting.SortManager;
 
 import java.util.List;
 
-public class FavouritesPresenter implements IProductsPresenter {
+public class FavouritesPresenter extends AdvertsPresenter {
 
-    private final AdvertsPresenterView view; //TODO maybe have to change this
-    private List<Advertisement> favourites;
-
-    public FavouritesPresenter(AdvertsPresenterView view) {
+    public FavouritesPresenter(AdvertsPresenterView view, DataModel dataModel) {
+        super(view, dataModel);
         this.view = view;
-        view.showLoadingScreen();
-        this.favourites = DataModel.getInstance().getUserFavourites();
-        view.hideLoadingScreen();
-    }
-
-
-    @Override
-    public void onBindThumbnailViewAtPosition(int position, ThumbnailView thumbnailView) {
-        Advertisement advert = favourites.get(position);
-        thumbnailView.setId(advert.getUniqueID());
-        thumbnailView.setTitle(advert.getTitle());
-        thumbnailView.setPrice(advert.getPrice());
-        if (advert.getImageUrl() != null) {
-            thumbnailView.setImageURL(advert.getImageUrl());
-        }
     }
 
     @Override
-    public int getItemCount() {
-        return favourites.size();
+    public List<Advertisement> getData() {
+        return super.dataModel.getUserFavourites();
     }
 
     @Override
-    public void onRowPressed(String uniqueIDOfAdvert) {
-        if (view == null) {
-            return;
-        }
-        if (canProceedWithTapAction()) {
-            view.showDetailsScreen(uniqueIDOfAdvert);
-        }
-    }
-
-    private boolean canProceedWithTapAction() {
-        return ClickDelayHelper.canProceedWithTapAction();
+    public List<Advertisement> sort(List<Advertisement> adverts) {
+        return SortManager.getInstance().sortWithDefaultSorting(adverts);
     }
 }
--- app/src/main/java/com/masthuggis/boki/view/FavoritesFragment.java
+++ app/src/main/java/com/masthuggis/boki/view/FavoritesFragment.java
@@ -1,92 +1,34 @@
 package com.masthuggis.boki.view;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.masthuggis.boki.R;
+import com.masthuggis.boki.injectors.DependencyInjector;
+import com.masthuggis.boki.presenter.AdvertsPresenter;
 import com.masthuggis.boki.presenter.AdvertsPresenterView;
 import com.masthuggis.boki.presenter.FavouritesPresenter;
-import com.masthuggis.boki.presenter.IProductsPresenter;
-import com.masthuggis.boki.utils.GridSpacingItemDecoration;
 
-public class FavoritesFragment extends Fragment implements AdvertsPresenterView {
+public class FavoritesFragment extends AdvertsView implements AdvertsPresenterView {
 
     private FavouritesPresenter presenter;
-    private ProductsRecyclerViewAdapter recyclerViewAdapter;
-    private View view;
-    ProgressBar progressBar;
-    RecyclerView recyclerView;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        this.view = inflater.inflate(R.layout.favorites_fragment, container, false);
-        initAssets();
-        initPresenter();
-        setupList();
-        return view;
-    }
-
-    private void initPresenter() {
-        this.presenter = new FavouritesPresenter(this);
-    }
-
-    private void initAssets() {
-        this.progressBar = view.findViewById(R.id.loadingProgressBar);
-        this.recyclerView = view.findViewById(R.id.favouritesRecyclerView);
-    }
-
-    private void setupList() {
-        recyclerViewAdapter = new ProductsRecyclerViewAdapter(getContext(), presenter); //TODO change this
-        recyclerView.setAdapter(recyclerViewAdapter);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, 40, true));
-    }
 
     @Override
-    public void showLoadingScreen() {
-        progressBar.setVisibility(View.VISIBLE);
-        recyclerView.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void hideLoadingScreen() {
-        progressBar.setVisibility(View.GONE);
-        recyclerView.setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    public void updateThumbnails() {
-        if (recyclerViewAdapter == null) {
-            setupList();
-        } else {
-            recyclerViewAdapter.notifyDataSetChanged();
+    protected AdvertsPresenter getPresenter() {
+        if (presenter == null) {
+            this.presenter = new FavouritesPresenter(this, DependencyInjector.injectDataModel());
         }
+        return presenter;
     }
 
     @Override
-    public void showNoThumbnailsAvailableScreen() {
+    protected View onCreateHeaderLayout() {
         // TODO: implement
+        return new TextView(getActivity());
     }
 
     @Override
-    public void hideNoThumbnailsAvailableScreen() {
+    protected View onCreateNoResultsFoundLayout() {
         // TODO: implement
-    }
-
-    @Override
-    public void showDetailsScreen(String id) {
-        Intent intent = new Intent(getContext(), DetailsActivity.class);
-        intent.putExtra("advertID", id);
-        startActivity(intent);
+        return new TextView(getActivity());
     }
 }
