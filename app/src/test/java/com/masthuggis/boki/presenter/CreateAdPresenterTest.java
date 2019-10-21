6b52b31d5f80f89543a05ed877bab2480f19268a
eilinn <elinmelander@gmail.com>
2019-10-21 16:41:12 +0200

    test createad

--- app/src/test/java/com/masthuggis/boki/presenter/CreateAdPresenterTest.java
+++ app/src/test/java/com/masthuggis/boki/presenter/CreateAdPresenterTest.java
@@ -2,10 +2,12 @@ package com.masthuggis.boki.presenter;
 
 import com.masthuggis.boki.R;
 import com.masthuggis.boki.injectors.DependencyInjector;
+import com.masthuggis.boki.model.AdFactory;
 import com.masthuggis.boki.model.Advert;
 import com.masthuggis.boki.model.Advertisement;
 import com.masthuggis.boki.model.Condition;
 import com.masthuggis.boki.model.DataModel;
+import com.masthuggis.boki.view.CreateAdActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,11 +18,16 @@ import org.mockito.junit.MockitoRule;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * Tests basic funtionality of CreateAdPresenter
@@ -30,12 +37,30 @@ import static org.junit.Assert.assertTrue;
 
 public class CreateAdPresenterTest {
 
-    CreateAdPresenter presenter = new CreateAdPresenter(new MockView(),
-            DependencyInjector.injectDataModel());
+    // CreateAdPresenter presenter = new CreateAdPresenter(new MockView(), DependencyInjector.injectDataModel());
+    @Mock
+    private CreateAdPresenter presenter;
+
+    @Mock
+    private CreateAdActivity CreateAdActivityMock;
     @Mock
     private DataModel databaseMock;
+
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    private List<Advertisement> tenAds = new ArrayList<>();
+
+    private List<Advertisement> generateAds() {
+        for (int i = 0; i < 9; i++) {
+            tenAds.add(AdFactory.createAd("datePublished",
+                    "uniqueOwner", "id", "title " + i,
+                    "", 100, Condition.GOOD, null, Collections.singletonList(""), "owner"));
+        }
+        return tenAds;
+    }
+
+    ;
 
     Advertisement advertisement = new Advert(
             "datePublished",
@@ -48,18 +73,24 @@ public class CreateAdPresenterTest {
             null,
             new ArrayList<>(),
             "owner");
-    //--------------------------------------------------------------------------
+
+    private void addAdToList(Advertisement ad) {
+        tenAds.add(ad);
+    }
 
     @Test
     public void testSaveAdvert() {
-        Mockito.doNothing().when(databaseMock).saveAdvert(null, advertisement);
-        assertEquals(databaseMock.getAdFromAdID("id"),advertisement);
+        CreateAdPresenter createAdPresenter = mock(CreateAdPresenter.class);
+        doNothing().when(createAdPresenter).saveAdvert();
+
+
+        //assertEquals(databaseMock.getAdFromAdID("id"),advertisement);
     }
 
     @Test
     public void testDeleteAdvert() {
-        Mockito.doNothing().when(databaseMock).saveAdvert(null, advertisement);
-        Mockito.doNothing().when(databaseMock).removeExistingAdvert(advertisement.getUniqueID(),advertisement.getUniqueOwnerID());
+        doNothing().when(databaseMock).saveAdvert(null, advertisement);
+        doNothing().when(databaseMock).removeExistingAdvert(advertisement.getUniqueID(), advertisement.getUniqueOwnerID());
         assertFalse(databaseMock.getAdFromAdID("id").equals(advertisement));
     }
 
@@ -68,26 +99,27 @@ public class CreateAdPresenterTest {
         advertisement.setTitle("updated title");
         advertisement.setPrice(100);
         advertisement.setDescription("updated description");
-        Mockito.doNothing().when(databaseMock).saveAdvert(null, advertisement);
-        Mockito.doNothing().when(databaseMock).updateAd(null, advertisement);
+        //doNothing().when(databaseMock).saveAdvert(null, advertisement);
+        doNothing().when(databaseMock).updateAd(null, advertisement);
 
-        assertEquals(advertisement.getTitle(), databaseMock.getAdFromAdID("id").getTitle());
+        assertEquals(advertisement.getTitle(), "updated title");
+        //verify(databaseMock, times(1)).);
+
     }
 
     @Test
     public void testTitleChanged() {
-        presenter.titleChanged("new title");
-        assertTrue(presenter.getAdvertisement().getTitle().equals("new title"));
-
+        String title = "new Title";
+        Mockito.doNothing().when(presenter).titleChanged(title);
+        verify(advertisement, times(1)).setTitle(title);
     }
 
     @Test
     public void testPriceChanged() {
-        presenter.priceChanged("800000");
-        assertEquals(0, presenter.getAdvertisement().getPrice());
+        String price = "300";
+        verify(advertisement, times(1)).setPrice(300);
 
-        presenter.priceChanged("300");
-        assertEquals(300, presenter.getAdvertisement().getPrice());
+
     }
 
     @Test
@@ -159,54 +191,72 @@ public class CreateAdPresenterTest {
             presenter = new CreateAdPresenter(this, DependencyInjector.injectDataModel());
         }
 
-        private CreateAdPresenter getPresenter() { return presenter; }
 
-        private boolean getPublishButtonEnabled() { return publishButtonEnabled; }
+        private CreateAdPresenter getPresenter() {
+            return presenter;
+        }
+
+        private boolean getPublishButtonEnabled() {
+            return publishButtonEnabled;
+        }
 
         @Override
         public void setTitle(String name) {
         }
+
         @Override
         public void setPrice(long price) {
 
         }
+
         @Override
         public void setImageUrl(String url) {
 
         }
+
         @Override
         public void setDescription(String description) {
 
         }
+
         @Override
         public void enablePublishButton(boolean isEnabled) {
 
         }
+
         @Override
         public void enableSaveButton(boolean b) {
 
         }
+
         @Override
         public void styleConditionButtonPressed(int condition) {
 
         }
+
         @Override
         public void setPreDefTagSelected(String tag, boolean isPressed) {
 
         }
+
         @Override
         public void displayUserTagButton(String tag) {
 
         }
+
         @Override
         public void removeUserTagButton(String tag) {
 
         }
-        @Override
-        public File getCurrentImageFile() { return null; }
 
         @Override
-        public void setTags(List<String> tags) { }
+        public File getCurrentImageFile() {
+            return null;
+        }
+
+        @Override
+        public void setTags(List<String> tags) {
+        }
 
         @Override
         public void toggleCondition(String id) {
@@ -219,3 +269,4 @@ public class CreateAdPresenterTest {
         }
     }
 }
+
