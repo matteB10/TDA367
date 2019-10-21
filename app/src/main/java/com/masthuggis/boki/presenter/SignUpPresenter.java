694ad896a8ab959c6a9ba04838e8fc17d3e73953
Joel Rudsberg <rudsberg@live.se>
2019-10-21 16:20:30 +0200

    Refactored so login/sigin is using the same abstraction/view interface for better design

--- app/src/main/java/com/masthuggis/boki/presenter/SignUpPresenter.java
+++ app/src/main/java/com/masthuggis/boki/presenter/SignUpPresenter.java
@@ -1,44 +1,49 @@
 package com.masthuggis.boki.presenter;
 
 import com.masthuggis.boki.model.DataModel;
-import com.masthuggis.boki.utils.FormHelper;
+import com.masthuggis.boki.view.ValidatorView;
 
-public class SignUpPresenter {
-    private View view;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Presenter dealing the sign up logic.
+ * @param <T>
+ */
+public class SignUpPresenter<T extends ValidatorView & SignUpPresenter.View> {
+    private ValidatorPresenter validator;
+    private T view;
     private DataModel dataModel;
-    public SignUpPresenter(View view, DataModel dataModel) {
+
+    public SignUpPresenter(T view, DataModel dataModel) {
         this.dataModel = dataModel;
         this.view = view;
+        validator = new ValidatorPresenter(view);
     }
 
     public void onSignInButtonPressed() {
         view.showSignInScreen();
     }
 
+    /**
+     * Asks validator to process data. If it is valid a request is made to the database. If it
+     * is valid view is notified and if not an error message will be shown.
+     * @param email
+     * @param password
+     * @param username
+     */
     public void onSignUpButtonPressed(String email, String password, String username) {
-        if (anyFieldIsBadlyFormatted(email, password, username)) {
-            view.showSignUpFailedMessage("Felaktig inmatning. Skrev du verkligen rätt?");
-            return;
+        List<String> dataToBeProcessed = new ArrayList<>();
+        dataToBeProcessed.add(email);
+        dataToBeProcessed.add(password);
+        dataToBeProcessed.add(username);
+
+        if (validator.onAccessRequested(dataToBeProcessed, "Felaktig inmatning. Skrev du verkligen rätt?")) {
+            dataModel.signUp(email, password, username, this.view::accessGranted, error -> view.showAccessFailedMessage(error));
         }
-        dataModel.signUp(email, password, username, this::onSignUpSuccess, (this::onSignUpFailed));
-    }
-
-    private boolean anyFieldIsBadlyFormatted(String email, String password, String username) {
-        FormHelper fh = FormHelper.getInstance();
-        return !fh.isValidEmail(email) || email.isEmpty() || password.isEmpty() || username.isEmpty();
-    }
-
-    private void onSignUpSuccess() {
-        view.signedIn();
-    }
-
-    private void onSignUpFailed(String errorMessage) {
-        view.showSignUpFailedMessage(errorMessage);
     }
 
     public interface View {
         void showSignInScreen();
-        void showSignUpFailedMessage(String message);
-        void signedIn();
     }
 }
