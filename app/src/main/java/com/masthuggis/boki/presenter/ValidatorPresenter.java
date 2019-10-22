3865f9c72d4f1b8fc0aebadbdc99dadc8241b11e
blomq <blomquistmatilda@hotmail.com>
2019-10-22 21:03:52 +0200

    changed access modifiers in validatorpresenter

--- app/src/main/java/com/masthuggis/boki/presenter/ValidatorPresenter.java
+++ app/src/main/java/com/masthuggis/boki/presenter/ValidatorPresenter.java
@@ -4,14 +4,14 @@ import com.masthuggis.boki.view.ValidatorView;
 
 import java.util.List;
 
-public class ValidatorPresenter {
+ class ValidatorPresenter {
     private ValidatorView view;
 
-    public ValidatorPresenter(ValidatorView view) {
+     ValidatorPresenter(ValidatorView view) {
         this.view = view;
     }
 
-    public boolean onAccessRequested(List<String> dataToBeValidated, String errorMessage)  {
+     boolean onAccessRequested(List<String> dataToBeValidated, String errorMessage)  {
         for (String data: dataToBeValidated) {
             if (data.isEmpty() || data == null) {
                 view.showAccessFailedMessage(errorMessage);
